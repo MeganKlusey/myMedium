826d0570fc9291afdce0d3f201c09fc5f2eed6eb
MeganKlusey <megan.klusey@hotmail.co.uk>
2020-12-13 23:06:24 +0000

    Add files via upload

---
+++ src/container/Write/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from '../../component/Header';
+import './style.css';
+
+/**
+* @author
+* @function Write
+**/
+
+const Write = (props) => {
+    return (
+        <div id='write'>
+            <Header />
+            <div id='writeBody'>
+                Write
+                <input />
+            </div>
+        </div>
+    )
+
+}
+
+export default Write
